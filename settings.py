9db310dc2e78a9b6a1a5d28fb69aa2991f423599
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2011-11-03 07:36:06 +0100

    fixed last commit - further removal of usage of REGISTRATION_TEMPLATE_DIR

--- settings.py
+++ settings.py
@@ -123,7 +123,6 @@ ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
-    REGISTRATION_TEMPLATE_DIR,
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
